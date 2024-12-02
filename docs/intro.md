1a282ea09f25e119b04a602f693f5f7c95ef578b
posicube-InHyuk <ihlim@posicube.com>
2024-12-02 17:55:13 +0900

    build/build_structure 1. Main 페이지 개발 2. 기술 블로그 구조 구축

--- docs/intro.md
+++ docs/intro.md
@@ -1,47 +1,11 @@
+# Tech Blog 개요
 ---
-sidebar_position: 1
----
+## 01_Company  
+**기업에서 근무하며 습득하게된 기술 스택 , R&D 이력을 기록합니다.**
+1. Tech Stack : 기술 스택 관련 내용이 서술되어 있습니다.   
+   1-1. Elastic Search
 
-# Tutorial Intro
-
-Let's discover **Docusaurus in less than 5 minutes**.
-
-## Getting Started
-
-Get started by **creating a new site**.
-
-Or **try Docusaurus immediately** with **[docusaurus.new](https://docusaurus.new)**.
-
-### What you'll need
-
-- [Node.js](https://nodejs.org/en/download/) version 18.0 or above:
-  - When installing Node.js, you are recommended to check all checkboxes related to dependencies.
-
-## Generate a new site
-
-Generate a new Docusaurus site using the **classic template**.
-
-The classic template will automatically be added to your project after you run the command:
-
-```bash
-npm init docusaurus@latest my-website classic
-```
-
-You can type this command into Command Prompt, Powershell, Terminal, or any other integrated terminal of your code editor.
-
-The command also installs all necessary dependencies you need to run Docusaurus.
-
-## Start your site
-
-Run the development server:
-
-```bash
-cd my-website
-npm run start
-```
-
-The `cd` command changes the directory you're working with. In order to work with your newly created Docusaurus site, you'll need to navigate the terminal there.
-
-The `npm run start` command builds your website locally and serves it through a development server, ready for you to view at http://localhost:3000/.
-
-Open `docs/intro.md` (this page) and edit some lines: the site **reloads automatically** and displays your changes.
+## 02_Study
+> 업무 외의 공부한 내용들을 기록합니다.
+1. Language : Programming 언어 관련 내용이 서술되어 있습니다.   
+   1-1. Java
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -4,58 +4,53 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
     title: string;
-    Svg?: React.ComponentType<React.ComponentProps<'svg'>>;
+    // Svg?: React.ComponentType<React.ComponentProps<'svg'>>;
     png?: string; // PNG 이미지 경로 추가
     description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
     {
-        title: 'About Me',
-        png: require('@site/static/img/01_introduce.png'),
+        title: 'Vision',
+        png: 'img/01_introduce_removebg.png',
         description: (
             <>
-                안녕하세요! 저는 서버 개발자 <strong>Plo</strong>입니다.
-                다양한 기술을 탐구하고, 문제를 해결하는 것을 좋아합니다.
-                이 사이트는 제 경험과 프로젝트를 소개하기 위한 공간입니다.
+                깊은 기술력을 가진
+                <br/>
+                <strong>엔지니어</strong>로써의 방향성을 지향하며
+                <br/>
+                꾸준한 성장을 지속중에 있습니다.
             </>
         ),
     },
     {
-        title: 'Tech Stack',
-        Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+        title: 'Tech',
+        png: 'img/02_tech_stack-removebg-preview.png',
         description: (
             <>
-                주로 <strong>Python</strong>, <strong>Java</strong>, <strong>React</strong>와 같은 기술을 사용하며,
-                클라우드 서비스와 데이터베이스 설계에 관심이 많습니다.
-                최신 기술과 트렌드를 지속적으로 학습하고 있습니다.
+                <strong>주 사용 기술 스택은 아래와 같습니다.</strong>
+                <br/>
+                Java , Spring
+                <br/>
+                Python ,Django , Flask , FastAPI
+                <br/>
+                Mysql , Postgresql
+                <br/>
+                <strong>아래의 Software를 개발하여 왔습니다.</strong>
+                <br/>
+                LMS, API Gateway ,WAS
+                <br/>
+                AI BackEnd , RAG
             </>
         ),
-    },
-    {
-        title: 'Projects',
-        Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-        description: (
-            <>
-                <ul>
-                    <li><strong>Project A:</strong> 효율적인 데이터 수집을 위한 자동화 솔루션</li>
-                    <li><strong>Project B:</strong> 클라우드 기반 API 개발</li>
-                    <li><strong>Project C:</strong> 사용자 친화적인 웹 애플리케이션 제작</li>
-                </ul>
-            </>
-        ),
-    },
+    }
 ];
 
-function Feature({title, Svg, png, description}: FeatureItem) {
+function Feature({title, png, description}: FeatureItem) {
     return (
-        <div className={clsx('col col--4')}>
+        <div className={clsx('col col--4 col--offset-1')}>
             <div className="text--center">
-                {Svg ? (
-                    <Svg className={styles.featureSvg} role="img" />
-                ) : png ? (
-                    <img className={styles.featureSvg} src={png} alt={title} /> // PNG 렌더링
-                ) : null}
+                <img src={png} alt={title}/>
             </div>
             <div className="text--center padding-horiz--md">
                 <Heading as="h3">{title}</Heading>
@@ -70,9 +65,8 @@ export default function HomepageFeatures(): JSX.Element {
         <section className={styles.features}>
             <div className="container">
                 <div className="text--center">
-                    <h3>안녕하세요! 저는 서버 개발자 <strong>Plo</strong>입니다.</h3>
-                    <h3>방문해주셔서 감사합니다,</h3>
-                    <h4>이 사이트는 저의 개발자로써 성장 과정을 소개하기 위한 공간입니다.</h4>
+                    <h2>안녕하세요!</h2>
+                    <h3>저는 서버 개발자 <strong>Plo</strong>입니다</h3>
                 </div>
                 <div className="row">
                     {FeatureList.map((props, idx) => (
